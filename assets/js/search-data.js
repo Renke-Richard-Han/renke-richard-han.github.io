35390130748f9c75e2dbf7ac64bc4a999ad1a431
Renke-Richard-Han <57907578+Renke-Richard-Han@users.noreply.github.com>
2026-01-07 21:48:26 +0000

    Deploying to gh-pages from @ Renke-Richard-Han/renke-richard-han.github.io@27943d862dab9b51727c4ec28224c61386e37295 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-publications",
           title: "Publications",
           description: "Only important publications are shown here, for all publication list, please refer to [google scholar](https://scholar.google.com/citations?user=10_G8DYAAAAJ&amp;hl=zh-CN).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
+          },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-projects",
           title: "Projects",

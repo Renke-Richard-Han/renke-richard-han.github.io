2f38f73121020da16b9847d8c35f9c53f7f88179
Renke-Richard-Han <57907578+Renke-Richard-Han@users.noreply.github.com>
2026-01-09 18:27:17 +0000

    Deploying to gh-pages from @ Renke-Richard-Han/renke-richard-han.github.io@9d0b610b41eee048b4443e66adcf526eeb49a832 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "This page is still under constructions, stay tuned!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";

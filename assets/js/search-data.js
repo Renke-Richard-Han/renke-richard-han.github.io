48601d02dfa2da907a82db7fcfd1ea044b13755e
Renke-Richard-Han <57907578+Renke-Richard-Han@users.noreply.github.com>
2026-01-09 12:06:13 +0000

    Deploying to gh-pages from @ Renke-Richard-Han/renke-richard-han.github.io@f19a18cdcc8794615f55a2c8cd54616372a010dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,11 +35,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-move-to-oxford-uk-joining-power-electronics-group-uni-of-oxford-as-post-doctoral-research-associate-under-prof-dan-rogers",
           title: 'Move to Oxford, UK, joining Power Electronics Group, Uni. of Oxford as Post-Doctoral...',
           description: "",
